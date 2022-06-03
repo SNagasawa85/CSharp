06438595022d719dfe66e21495e8e5f2bfd50017
Stephen Nagasawa <stephen.nagasawa@me.com>
2022-06-03 15:24:08 -0600

    new assignments

---
+++ fundamentalsI/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+for (int i = 1; i <= 255; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 1; i <= 100; i++)
+{
+    if ( i%15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    if ( i%3 == 0 && i%5 != 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    if ( i%5 == 0 && i%3 !=0)
+    {
+        Console.WriteLine("Buzz");
+    }
+}
+
+
+
+
+
