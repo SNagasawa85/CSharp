a50eba0ff076022fab3aef866f1cb68aa963cc61
Stephen Nagasawa <stephen.nagasawa@me.com>
2022-06-03 14:26:03 -0600

    initial csharp commit

---
+++ NewConsoleProj/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// variable to interpolate
+string place = "Coding Dojo";
+
+// Interpolated string
+string message = $"Hello {place}";
+
+Console.WriteLine(message);
+
