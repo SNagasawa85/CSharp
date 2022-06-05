33f6f4af7c284927eb263af2b96e54de42bf4407
Stephen Nagasawa <stephen.nagasawa@me.com>
2022-06-05 13:46:55 -0600

    new practice assignment added

---
+++ Human/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Human
+{
+    public string Name;
+    public int Strength;
+    public int Intelligence;
+    public int Dexterity;
+    public int Health;
+
+    public string SetName(string NameVal)
+    {
+        Name = NameVal;
+        Strength = 3;
+        Intelligence = 3;
+        Dexterity = 3;
+        Health = 100;
+        return $"{NameVal} has been created!";
+    }
+
+    public void CreateCustom(string NameVal, int Str, int Intel, int Dex, int Health)
+    {
+        Name = NameVal;
+        Strength = Str;
+        Intelligence = Intel;
+        Dexterity = Dex;
+        Health = Health;
+    }
+
+    public int Attack(Human target)
+    {
+        int damage = Human.Strength * 5;
+        target.Health -= damage;
+        return target.Health;
+    }
+
+}
+
+
